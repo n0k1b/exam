122447cdf3aaedd6c0a72a5bed13690364472ce2
n0k1b <nokibevon7@gmail.com>
2020-08-10 14:42:58 +0600

    subscription notification updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -100,7 +100,7 @@ class UserController extends Controller {
         //file_put_contents('ttest_subscription.txt','Hello');
     $sender = new SMSSender("https://developer.bdapps.com/sms/send", $this->app_id,$this->app_password);
      $receiver 	= new SubscriptionReceiver();
-     file_put_contents('ttt.txt',"hello ".$receiver);
+     file_put_contents('ttt.txt',"hello");
     // $frequency = $receiver->getFrequency();
      $status = $receiver->getStatus();
     
