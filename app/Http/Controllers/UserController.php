6559b8675cf2f10da1b9b303847e9ac62eb56476
n0k1b <nokibevon7@gmail.com>
2020-08-19 11:14:17 +0600

    App url changed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -129,7 +129,7 @@ class UserController extends Controller {
      //user::where('mobile','=',"tel:".$address)->update(['status'=>$status]);
     
     
-    $sender->sms("Download the app. https://rb.gy/umd6b1","tel:88".$address);
+    $sender->sms("Download the app. https://play.google.com/store/apps/details?id=xit.zubrein.eexam","tel:88".$address);
     }
     public function subscription_free(Request $request) {
         $user_id = $request->user_id;
